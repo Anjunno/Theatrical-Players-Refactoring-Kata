b3218abcfb040a736d5dedce42460eaa55040272
joon <joon@teamo2.kr>
2026-07-04 18:03:33 +0900

    refactor: totalVolumeCredits 함수 추출

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -1,33 +1,32 @@
 package theatricalplays;
 
 import java.text.NumberFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class StatementPrinter {
 
-    private  Map<String, Play> plays;
+    private Map<String, Play> plays;
 
     public String print(Invoice invoice, Map<String, Play> plays) {
         this.plays = plays;
         var totalAmount = 0;
-        var volumeCredits = 0;
         var result = String.format("Statement for %s%n", invoice.customer);
 
         for (var perf : invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
-
             // print line for this order
             result += String.format("  %s: %s (%s seats)%n", playFor(perf).name, usd(amountFor(perf)), perf.audience);
             totalAmount += amountFor(perf);
         }
+
         result += String.format("Amount owed is %s%n", usd(totalAmount));
-        result += String.format("You earned %s credits%n", volumeCredits);
+        result += String.format("You earned %s credits%n", totalVolumeCredits(invoice.performances));
         return result;
     }
 
     private static String usd(int aNumber) {
-        return NumberFormat.getCurrencyInstance(Locale.US).format(aNumber/ 100);
+        return NumberFormat.getCurrencyInstance(Locale.US).format(aNumber / 100);
     }
 
     private int amountFor(Performance aPerformance) {
@@ -54,13 +53,21 @@ public class StatementPrinter {
     }
 
     private Play playFor(Performance aPerformance) {
-        return  plays.get(aPerformance.playID);
+        return plays.get(aPerformance.playID);
     }
 
     private int volumeCreditsFor(Performance aPerformance) {
         int result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
         if ("comedy".equals(playFor(aPerformance).type)) result += Math.floor(aPerformance.audience / 5);
-        return  result;
+        return result;
+    }
+
+    private int totalVolumeCredits(List<Performance> performances) {
+        var volumeCredits = 0;
+        for (var perf : performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
     }
 }
